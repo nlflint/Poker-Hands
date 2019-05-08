c40b9682e0155338ba62f4999bfa3935c2fb70df
Nathan Flint <nlflint@gmail.com>
2019-05-07 23:27:12 -0700

    More progress and bug fixes

--- Poker-hands.rkt
+++ Poker-hands.rkt
@@ -42,40 +42,52 @@
 (define (match-rec card-definitions cards previous-card matched-cards)
   (if (empty? card-definitions)
       (list (list matched-cards cards))
-      (map
-       (lambda (card) (first-or-default (match-rec (cdr card-definitions) (remove card cards) card (cons card matched-cards))))
-       (filter (make-card-predicate (first card-definitions) previous-card) cards))))
+      (let ([next-possible-cards (filter (make-card-predicate (first card-definitions) previous-card) cards)])
+        (if (empty? next-possible-cards)
+            (list (list `() (append matched-cards cards)))
+            (map (lambda (card) (first-or-default (match-rec (cdr card-definitions) (remove card cards) card (cons card matched-cards)))) next-possible-cards)))))
 
 (define (first-or-default matches)
   (cond
     [(empty? matches) `()]
     [(empty? (first matches)) (first-or-default (cdr matches))]
     [else (first matches)]))
-      
-
-(null? (cdr (list 1)))
 
 (define (match-hand hand-definitions hand)
   (first-or-default (match-rec hand-definitions (cards hand) `() `())))
 
-(define (identify-hand-rec pattern cards)
+(define (identify-hand-rec pattern hand)
   (if (empty? pattern)
-      `(() cards)
-      (let ([matches (match-hand (first pattern) cards)])
-        (list
-         (append (first matches) (identify-hand-rec (cdr pattern) (second matches)))
-         (second matches)))))
+      null
+      (let ([match-result (match-hand (first pattern) hand)])
+        
+         (let ([rec (identify-hand-rec (cdr pattern) (make-hand (name hand) (second match-result)))]
+               [matches (first match-result)])
+           (cons matches rec)))))
 
-(define (identify-hand cards)
+(define and-l (lambda x 
+    (if (null? x)
+        #t
+        (if (car x) (apply and-l (cdr x)) #f))))
+
+(define (is-match? match-result)
+  (let* ([pattern-matches (second match-result)]
+         [empty-projection (map pair? pattern-matches)]
+         [result (apply and-l empty-projection)]
+         [some-list (list 1 2 3)])
+  result))
+  
+(define (identify-hand hand)
   (first-or-default
    (map (lambda (pattern)
           (let
-            ([matching-cards (list (first pattern) (identify-hand-rec (cdr pattern) cards))])
+            ([match-result (list (first pattern) (identify-hand-rec (cdr pattern) hand))])
             (cond
-              [(empty? (second matching-cards)) null]
-               [else (first matching-cards)]))) hand-patterns)))
+              [(is-match? match-result) (first match-result)]
+              [else null])))
+        hand-patterns)))
 
-(define hand cons)
+(define make-hand cons)
 (define name first)
 (define cards cdr)
 
@@ -85,19 +97,34 @@
 (check-true ((make-value-predicate `any `(2 Hearts)) `(3 Clubs)))
 (check-true ((make-suit-predicate `any `(2 Hearts)) `(3 Clubs)))
 (check-true ((make-card-predicate `(any any) `(10 Spades)) `(5 Diamonds)))
-(check-equal?
- (match-hand (second one-pair-definition) (hand "Nate" `((2 Hearts) (3 Hearts) (2 Clubs))))
- `(((2 Clubs) (2 Hearts)) ((3 Hearts))))
-;(check-equal? (match-hand (second one-pair-definition) `((10 Hearts) (9 Hearts) (6 Clubs))) `(() ((10 Hearts) (9 Hearts) (6 Clubs))))
-(match-hand (second one-pair-definition) (hand "Nate" `((10 Hearts) (9 Hearts) (6 Clubs))))
-;(match-hand (second one-pair-definition) `((2 Hearts) (3 Hearts) (2 Clubs) (2 Diamonds)))
-;(match-hand (second three-of-a-kind) `((2 Hearts) (3 Hearts) (2 Clubs) (2 Diamonds)))
-;(identify-hand `((2 Hearts) (2 Hearts)))
-;(identify-hand `((2 Hearts) (2 Hearts) (2 Spades) (3 Hearts)))
-;(identify-hand `((14 Hearts) (14 Hearts) (14 Spades) (14 Hearts)))
-;(identify-hand `((13 Hearts) (13 Hearts) (14 Spades) (14 Hearts)))
-;(identify-hand `((13 Hearts) (13 Hearts) (14 Spades) (14 Hearts) (14 Hearts)))
+(check-false (apply and-l `(#f #f #t)))
+(check-false (apply and-l `(#f #t)))
+(check-true (apply and-l `(#t #t)))
+;(check-equal?
+;(match-hand (second one-pair-definition) (make-hand "Nate" `((2 Hearts) (3 Hearts) (2 Clubs))))
+; `(((2 Clubs) (2 Hearts)) ((3 Hearts))))
+;(check-equal? (match-hand (second one-pair-definition) (make-hand "nate" `((10 Hearts) (9 Hearts) (6 Clubs)))) `(() ((10 Hearts) (9 Hearts) (6 Clubs))))
+;(match-hand (second one-pair-definition) (make-hand "nate" `((2 Hearts) (3 Hearts) (2 Clubs) (2 Diamonds))))
+;(match-hand (second three-of-a-kind) (make-hand "nate" `((2 Hearts) (3 Hearts) (2 Clubs) (2 Diamonds))))
+;(match-hand (second three-of-a-kind) (make-hand "nate" `((2 Hearts) (3 Hearts) (2 Clubs) (2 Diamonds))))
+(match-hand (second (first hand-patterns)) (make-hand "nate" `((6 Hearts) (3 Hearts) (4 Hearts) (5 Hearts) (2 Hearts))))
+;(identify-hand (make-hand "nate" `((2 Hearts) (2 Hearts))))
+;(identify-hand (make-hand "nate" `((2 Hearts) (2 Hearts) (2 Spades) (3 Hearts))))
+;(identify-hand (make-hand "nate" `((14 Hearts) (14 Hearts) (14 Spades) (14 Hearts))))
+;(identify-hand (make-hand "joe" `((13 Hearts) (13 Hearts) (14 Spades) (14 Hearts))))
+;(identify-hand (make-hand "joe" `((13 Hearts) (12 Hearts) (11 Hearts) (2 Hearts) (6 Hearts))));
+;(identify-hand (make-hand "joe" `((13 Hearts) (12 Hearts) (11 Hearts) (10 Hearts) (9 Hearts))))
+;(identify-hand (make-hand "dude" `((13 Hearts) (13 Hearts) (14 Spades) (14 Hearts) (14 Hearts))))
 
+;(define two-pair-pattern `("Two pair" ((any any) (equal any)) ((any any) (equal any))))
+;(identify-hand-rec (cdr two-pair-pattern) (make-hand "nates" `((2 Hearts)(2 Clubs))))
+
+;(define full-house-pattern `("Full house" ((any any) (equal any) (equal any)) ((any any) (equal any))))
+;(identify-hand-rec (cdr full-house-pattern) (make-hand "nates" `((2 Hearts)(2 Clubs))))
+
+;(identify-hand-rec (cdr (first hand-patterns)) (make-hand "nates" `((6 Hearts) (3 Hearts) (4 Hearts) (5 Hearts) (2 Hearts))))
+
+;(append `((2 Hearts) (2 Clubs)) null)
 ;(check-equal?
 ; (first (match-it one-pair-definition `((2 Hearts) (3 Hearts) (2 Clubs)) `())
 ; `((2 Hearts) (2 Clubs))))
